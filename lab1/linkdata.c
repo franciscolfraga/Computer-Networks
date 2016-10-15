ae6cbc2e1f669d0746ad485838cc321bbb1c0274
franciscolfraga <franciscolfraga@gmail.com>
2016-10-15 20:38:02 +0100

    worked on llopen and functions, continue next time

--- lab1/linkdata.c
+++ lab1/linkdata.c
@@ -17,23 +17,24 @@ void setuplink(char* sport, int sportfd,int id){
 		exit(-1);
 	}
 
-
+	//ver boas práticas e mudar
 	//setup da struct
-	linkinfo.port=sport;
-	linkinfo.baudRate=4800; //default minha (depois costumizar)
-	linkinfo.sequenceNumber=0;
-	linkinfo.timeout=1;
-	linkinfo.numTransmissions=3;
+	linkinfo.port = sport;
+	linkinfo.baudRate = 4800; //default minha (depois costumizar)
+	linkinfo.sequenceNumber = 0;
+	linkinfo.timeout = 1;
+	linkinfo.numTransmissions = 3;
 	printf("\tAll set in data link layer info!\n");
-	if(llopen(sportfd,id)<0){
+	if(llopen(sportfd , id)<0){
 		printf("\n\tError setting up llopen!\n");
+		exit(-1)
 	}
 
 }
 int setupTermios(int fd){
 	//save old serial port settings, add control later
 	printf("\tSaving current settings of termios...\n");
-	tcgetattr(fd, &oldtio);
+	tcgetattr(fd , &oldtio);
 	printf("\tOld termios settings saved!\n");
 	//setup of new Termios (asynchronous as asked)
 	printf("\tSetting up new termios...\n");
@@ -42,14 +43,89 @@ int setupTermios(int fd){
     newtio.c_iflag = IGNPAR | ICRNL;
     newtio.c_oflag = 0;
     newtio.c_lflag = ICANON;
-    newtio.c_cc[VMIN]=1;
-    newtio.c_cc[VTIME]=0;
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd,TCSANOW,&newtio);
+    newtio.c_cc[VMIN] = 1;
+    newtio.c_cc[VTIME] = 0;
+    tcflush(fd , TCIFLUSH);
+    tcsetattr(fd , TCSANOW,&newtio);
     printf("\tNew termios setup done!\n");
     //add control later
     return 1;
 }
-int llopen(int fd, int id){
-	return 1;
+int llopen(int fd , int id){
+	int counter = 0;
+		if(id == SENDER){
+			//add alarm later
+			while(counter < linkinfo.numTransmissions){
+				counter++;
+			}
+		}
+	return -1;
+}
+int sendcmd(int fd, int frametype, int id) {
+	unsigned char frame[FRAME_SIZE];
+
+	frame[0] = FLAG;
+	frame[4] = FLAG;
+
+	switch(frametype) {
+		case 1:
+		//SET
+			frame[1] = getA(1,id);
+			frame[2] = CSET;
+			frame[3] = frame[1] ^ frame[2];
+			break;
+		case 2:
+		//UA
+			frame[1] = getA(2,id);
+			frame[2] = CUA;
+			frame[3] = frame[1] ^ frame[2];
+			break;
+		case 3:
+		//DISC
+			frame[1] = getA(1,id);
+			frame[2] = CDISC;
+			frame[3] = frame[1] ^ frame[2];
+			break;
+		//ver rr e rej
+		default:
+			printf("\tERROR sending frame (unexpected frame)\n");
+			exit(-1);
+			break;
+	}
+
+	if(frame[1] == 0) {
+		printf("\tERROR sending frame (unnexpected status)\n");
+		exit(-1);
+	}
+
+	if (write(fd, frame, FRAME_SIZE) != FRAME_SIZE) {
+		printf("\tERROR sending frame (size don't match)\n");
+		exit(-1);
+	}
+
+	return 0;
+}
+
+unsigned char getA(int type,int id){
+	switch(type){
+		case 1:
+			if(id==SENDER)
+				return A03;
+			else if(id==RECEIVER)
+				return A01;
+			else{
+				printf("\tERROR setting up frame\n");
+				exit(-1);
+			}
+		case 2:
+			if(id==SENDER)
+				return A01;
+			else if(id==RECEIVER)
+				return A03;
+			else{
+				printf("\tERROR setting up frame\n");
+				exit(-1);
+			}
+	}
+	return 0;
 }
--- lab1/linkdata.h
+++ lab1/linkdata.h
@@ -1,8 +1,26 @@
 #ifndef LINKDATA_H_   /* Include guard */
 #define LINKDATA_H_
 #define MAX_SIZE 255
+#define SENDER 1
+#define RECEIVER 2
+
+#define CSET 0x03
+#define CUA 0x07
+#define CRR 0x05
+#define CREJ 0x01
+#define CDISC 0x0B
+
+#define FLAG 0x7E
+#define A03 0x03
+#define A01 0x01
+#define ESCAPE 0x7D
+
+#define FRAME_SIZE 5
+
 void setuplink(char* sport, int sportfd,int id);
+
 int llopen(int fd, int id);
+
 typedef struct { 
 
 char* port; /*Dispositivo /dev/ttySx, x = 0, 1*/ 
@@ -19,6 +37,11 @@ char frame[MAX_SIZE]; /*Trama*/
 
 }  linklayer;
 linklayer linkinfo;
+
 int setupTermios(int fd);
+
+unsigned char getA(int type,int id);
+
+int sendcmd(int fd, int frametype, int id);
 
 #endif // LINKDATA_H_
